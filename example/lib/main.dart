0ebc1d45e4a9bb2b42e2a5bfbb89295d960b297e
iFlyCai <zhangchengcai@aerogia.com>
2020-07-15 17:04:57 +0800

    本次更新UI删除无用的代码类

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'src/item_container.dart';
 import 'view/show_city_picker.dart';
 import 'view/wip.dart';
-import 'view/show_full_page_picker.dart';
-import 'view/util_getLocationInfo.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,9 +22,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
         '/name': (_) => new ShowCityPicker(),
-        '/full_page': (_) => new ShowFullPageCityPicker(),
-        '/city_select': (_) => new WorkInProgress(),
-        '/util_getLocationInfo': (_) => new UtilGetLocationInfo()
       },
       home: Body(),
     );
@@ -44,21 +39,6 @@ class _BodyState extends State<Body> {
       "icon": Icons.place,
       "name": "ios选择器",
       "routerName": '/name'
-    },
-    {
-      "icon": Icons.fullscreen,
-      "name": "三级全屏选择器",
-      "routerName": '/full_page'
-    },
-    {
-      "icon": Icons.location_city,
-      "name": "城市选择器",
-      "routerName": '/city_select'
-    },
-    {
-      "icon": Icons.location_city,
-      "name": "内置工具类",
-      "routerName": '/util_getLocationInfo'
     },
   ];
 
